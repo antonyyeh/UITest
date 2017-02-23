5baf281c3b256b4bd6a8f032b765254170320258
antony.yeh <antony.yeh@fuhu.com>
2017-02-23 13:42:04 +0800

    Initial Commit

---
+++ UITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITest
+//
+//  Created by antony.yeh on 2017/2/23.
+//  Copyright © 2017年 antony.yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
